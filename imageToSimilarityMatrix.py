2b2b3fcc9eec937ce206e2223ee74e1b03a894bc
luca590 <lucasgaylord@protonmail.com>
2017-12-05 22:51:51 +0800

    .

--- imageToSimilarityMatrix.py
+++ imageToSimilarityMatrix.py
@@ -2,8 +2,16 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
+number_of_images = 5
+matrix_of_similarities = []
 
+for i in range(number_of_images):
 
+	#input image into model here
+
+	#get last layer of model here
+
+	
 
 
 
