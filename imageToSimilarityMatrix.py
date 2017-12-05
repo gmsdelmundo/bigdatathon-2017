a00aceb32abb4d26a97a9e9cfc690a550b744fe1
luca590 <lucasgaylord@protonmail.com>
2017-12-05 22:43:47 +0800

    ..

--- imageToSimilarityMatrix.py
+++ imageToSimilarityMatrix.py
@@ -5,3 +5,9 @@ from torchvision import models
 
 
 
+
+
+#TODO:
+#Lucas - Feed images from file into Inception model
+#Hilda - Set up model and get last layer of it (called feature vector)
+#Tushar - Take feature vectors and 
