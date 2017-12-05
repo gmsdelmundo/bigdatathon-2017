20e40a2035faa26754692f87adbe255b362f30d7
luca590 <lucasgaylord@protonmail.com>
2017-12-05 23:24:24 +0800

    ..

--- imageToSimilarityMatrix.py
+++ imageToSimilarityMatrix.py
@@ -4,22 +4,20 @@ from torchvision import models
 import numpy as np
 
 number_of_images = 5
-matrix_of_similarities = np.zeros(shape=(500,500))
+matrix_of_similarities = np.zeros(shape=(number_of_images, number_of_images))
 
 test_vector = [1,2,3,4,5]
 feature_vector = []
 
 for i in range(number_of_images):
 	for j in range (number_of_images):
-		j += i
-
 
 	#input image into model here
 
 	#get last layer of model here
 		
-		print("i is: " + i + " and j is: " + j)
-		#matrix_of_similarities[i][j] = 10
+	matrix_of_similarities[i][j] = #calculate similarity score here
+	
 
 
 
